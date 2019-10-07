07ce84e03273505a1d69aeb663ce14ca5ec70939
yousefa4647.github.io <43858664+yousefa4647@users.noreply.github.com>
2019-10-07 10:29:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Yousef A"
+let userName = "yousefa4647";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
